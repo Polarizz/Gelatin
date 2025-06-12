b0b3ff4a12e6f8ce411bb12b227a139d5b2ccfaa
polarizz <polarizz39@gmail.com>
2025-06-12 01:21:31 -0700

    Import demo project

---
+++ Examples/GelatinDemo/GelatinDemo/GelatinDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GelatinDemoApp.swift
+//  GelatinDemo
+//
+//  Created by Paul Wong on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GelatinDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
